3ec01bfe7b480ea4c006244b2ee159b69790eb04
Manuel Pérez <Manolitopc89@gmail.com>
2024-04-30 20:41:59 -0500

    Actualizando el first commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,43 +1,41 @@
-import java.util.Collections;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Principal {
+
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Escriba el límite de la tarjeta: ");
-        double limite = lectura.nextDouble();
-        TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
+        MonedaAConsultar consulta = new MonedaAConsultar();
+        System.out.println("\n************************************");
+        System.out.println("\nSeleccione la opción de la conversión que desea realizar: ");
+        System.out.println("""
+                1. USD >> COP Dolar a Peso Colombiano.
+                2. COP >> USD Peso Colombiano a Dolar.
+                3. EUR >> COP Euro a Peso Colombiano.
+                4. COP >> EUR Peso Colombiano a Euro.
+                5. CLP >> COP Peso Chileno a Peso Colombiano.
+                6. COP >> CLP Peso Colombiano a Peso Chileno.
+                7. BRL >> COP Real Brasileño a Peso Colombiano.
+                8. COP >> BRL Peso Colombiano a Real Brasileño.
+                9. ARS >> COP Peso Argentino a Peso Colombiano.
+                10. COP >> ARS Peso Colombiano a Peso Argentino.
+               """);
 
-        int salir = 1;
-        while(salir != 0) {
-            System.out.println("Escriba la descripción de la compra:");
-            String descripcion = lectura.next();
-
-            System.out.println("Escriba el valor de la compra:");
-            double valor = Double.valueOf(lectura.next());
-
-            compra Compra = new compra(valor, descripcion);
-            boolean compraRealizada = tarjeta.lanzarCompra(Compra);
-
-            if (compraRealizada) {
-                System.out.println("Compra realizada!");
-                System.out.println("Escriba 0 para salir o 1 para continuar");
-                salir = lectura.nextInt();
-            } else {
-                System.out.println("Saldo insuficiente!");
-                salir = 0;
-            }
+        try {
+            var StringDeConversion = (lectura.nextLine());
+            Moneda moneda = consulta.buscaMoneda(StringDeConversion);
+            System.out.println(moneda);
+            GeneradorDeArchivos generador = new GeneradorDeArchivos();
+            generador.guardarJson(moneda);
+        }catch (NumberFormatException e) {
+            System.out.println("Número no encontrado. "+ e.getMessage());
+        }catch (RuntimeException | IOException e) {
+            System.out.println(e.getMessage());
+            System.out.println("Finalizado la aplicación...");
         }
-        System.out.println("***********************");
-        System.out.println("COMPRAS REALIZADAS:\n");
 
-        Collections.sort(tarjeta.getListaDeCompras());
-
-        for (compra Compra : tarjeta.getListaDeCompras()) {
-            System.out.println(Compra.getDescripcion() + " - " +Compra.getValor());
-        }
-        System.out.println("\n***********************");
-        System.out.println("\nSaldo de la tarjeta: " +tarjeta.getSaldo());
 
     }
+
+
 }
